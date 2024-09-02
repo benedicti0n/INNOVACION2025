4c2106ee94fe1db84e2ebd8b6da752739826d20c
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-09-03 05:18:59 +0530

    homepage layout test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import StarryBackground from "./components/StarryBackground/StarryBackground"
+import Navbar from "./components/Navbar/Navbar"
 function App() {
 
   return (
-    <div>
+    <div className="h-screen w-full">
       <StarryBackground />
+
+      <div className="h-full lg:mx-40 lg:my-5">
+        <Navbar />
+      </div>
     </div>
   )
 }
