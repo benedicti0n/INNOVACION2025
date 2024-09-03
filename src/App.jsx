fe226dce087e1a177d7bb8dcb534b68787806400
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-09-03 08:01:34 +0530

    homepage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import StarryBackground from "./components/StarryBackground/StarryBackground"
 import Navbar from "./components/Navbar/Navbar"
+import Homepage from "./components/Homepage/Homepage"
+
 function App() {
 
   return (
     <div className="h-screen w-full">
+      {/* will act as an fixed background */}
       <StarryBackground />
 
+      {/* main sections which will scroll through smooth scroll */}
       <div className="h-full lg:mx-40 lg:my-5">
+        {/* navbar will stay fixed */}
         <Navbar />
+
+        <Homepage />
+
+        {/* About us and events section */}
+        <div className="h-screen w-full">
+          <h1>event section</h1>
+        </div>
       </div>
     </div>
   )
---
+++ src/components/Homepage/Homepage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import RegisterBtn from "../RegisterBtn/RegisterBtn"
+
+const Homepage = () => {
+    return (
+        // Home page 
+        < div className="homepage h-full flex justify-center items-center flex-col pb-36" >
+            <h1 className="text-8xl text-white font-[Quantum] pb-11">INNOVACIÓN</h1>
+            <RegisterBtn />
+        </ div >
+
+    )
+}
+
+export default Homepage
