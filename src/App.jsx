2e1005880cb2b64337e1524b4b474adbe4d203fa
Ashesh Bandopadhyay <lbandopadhyay2@gmail.com>
2024-09-11 11:23:01 +0530

    layout simplification

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import Navbar from "./components/Navbar/Navbar"
 import Homepage from "./components/Homepage/Homepage"
-import StarryBackground from "./components/StarryBackground/StarryBackground"
 import Eventspage from "./components/Eventpage/Eventspage"
+import GradientBox from "./components/GradientBox/GradientBox"
 
 function App() {
 
   return (
-    <div className="h-screen w-full">
-      <StarryBackground extraClass="bg-[radial-gradient(circle_at_bottom,#16423C_0%,#000_100%)]" />
-      <div className="h-full lg:mx-40 lg:py-5">
-        <Navbar />
-
-        <Homepage />
-
-      </div>
-
+    <div className="w-full">
+      <Navbar />
+      <Homepage />
       <Eventspage />
     </div>
   )
--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -5,7 +5,8 @@ import RegisterBtn from "../RegisterBtn/RegisterBtn"
 const Homepage = () => {
     return (
         // Home page
-        <div className="h-full flex justify-center items-center flex-col pb-36">
+        <div className="h-screen flex justify-center items-center flex-col pb-36 lg:mx-40 lg:py-5">
+            <StarryBackground extraClass="bg-[radial-gradient(circle_at_bottom,#16423C_0%,#000_100%)]" />
             {/* <StarryBackground /> */}
             <h1 className="text-8xl text-white font-[Quantum] pb-11">INNOVACIÓN</h1>
             <RegisterBtn />
